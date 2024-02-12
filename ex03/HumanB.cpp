fb040a5dd39eda215c9b22dd2f9bb4e35a777c47
osarsari <o.sarsari@gmail.com>
2024-02-12 18:56:56 +0100

    fix ex03

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: osarsari <osarsari@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 08:52:18 by osarsari          #+#    #+#             */
-/*   Updated: 2023/12/19 08:52:18 by osarsari         ###   ########.fr       */
+/*   Updated: 2024/02/12 18:56:43 by osarsari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : _name(name)
+HumanB::HumanB(std::string name) : _name(name), _weapon(NULL)
 {
 	return ;
 }
@@ -24,6 +24,11 @@ HumanB::~HumanB(void)
 
 void	HumanB::attack(void) const
 {
+	if (this->_weapon == NULL)
+	{
+		std::cout << this->_name << " attacks with his bare hands" << std::endl;
+		return ;
+	}
 	std::cout << this->_name << " attacks with his " << \
 		this->_weapon->getType() << std::endl;
 	return ;
